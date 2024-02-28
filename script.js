d4fbb15a0cd45b1fee820e3cca09222881140c87
Alex-N40 <alex_n40@proton.me>
2024-02-28 13:45:52 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Write your JavaScript below this line
+var firstName = "Alex";
+var favoriteSnack = "cheese and fruit";
+console.log(
+  "Hi, my name is " +
+    firstName +
+    " and I wish I had " +
+    favoriteSnack +
+    " right now."
+);
+
+// Result in console: Hi, my name is Alex and I wish I had cheese and fruit right now.
